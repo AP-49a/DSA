fdf65e239d68f3af2bac25fa0d1a12475d92936d
AP-49a <it2025049@rcciit.org.in>
2026-07-26 15:00:05 +0530

    traversed a 3 variable linked list.

---
+++ linked lists/travlink.c
@@ -0,0 +1,43 @@
+//traversal of a linked list.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+int main()
+{
+    struct node *head, *second, *third;
+
+    head = malloc(sizeof(struct node));
+    second = malloc(sizeof(struct node));
+    third = malloc(sizeof(struct node));
+
+    head -> data = 10;
+    head -> next = second;
+
+    second -> data = 20;
+    second -> next = third;
+
+    third -> data = 30;
+    third -> next = NULL;
+
+    struct node *temp = head;
+
+    while(temp!=NULL)
+    {
+        printf("%d\n", temp->data);
+        temp = temp -> next;
+    }
+
+    free(head);
+    free(second);
+    free(third);
+
+    return 0;
+
+}
